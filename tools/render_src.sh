8c3e02ff58b8a9516937392175d7c7345a693959
Bereket Abraham <babraham@figma.com>
2025-07-10 16:11:38 -0500

    small tweaks to make mkdocs build

--- tools/render_src.sh
+++ tools/render_src.sh
@@ -5,13 +5,18 @@
 #   cd ~/project/dir
 #   tools/render_src.sh
 
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/opt/homebrew/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PATH="/usr/local/bin:/opt/homebrew/bin:$PATH"
+export DYLD_LIBRARY_PATH="/usr/local/lib:/opt/homebrew/lib:$DYLD_LIBRARY_PATH"
+
 set -euo pipefail
 
+rm -rf assets/*
 cp -R src/error assets
 cp -R src/wifi assets
 
 pushd src/www
-mkdocs build -d ../assets/www
+mkdocs build -d ../../assets/www
 popd
 
 echo "Rendered the site's assets into $(pwd)/assets"
